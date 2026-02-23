7bc484e82acd6c02840e7096e0321c46500dc70b
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-02-23 12:41:34 +0700

    p

---
+++ .vscode/4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama, alamat;
+
+    cout <<"identitas" <<endl;
+
+    cout <<"nama lengkap : ";
+    getline(cin,nama):
+
+    cout << "alamat : ";
+    getline(cin,alamat);
+
+    cout << "\n hasil"<< endl;}
